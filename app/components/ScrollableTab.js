868e6abd89397445076fefbac555a3f5024710b9
Max Wang <wrymax@qq.com>
2019-04-12 13:20:34 -0400

    added style for scrollbar

--- app/components/ScrollableTab.js
+++ app/components/ScrollableTab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import {
-  Text, StyleSheet
+  Text, StyleSheet, View
 } from 'react-native';
 
 import ScrollableTabView,{DefaultTabBar,ScrollableTabBar}  from 'react-native-scrollable-tab-view';
@@ -11,30 +11,37 @@ class ScrollableTab extends React.Component {
 
     render() {
         return (
-            
-            <ScrollableTabView
-                style={styles.ScrollableTabView}
-                // initialPage={0}
-                renderTabBar={() => <ScrollableTabBar />}
-                tabBarPosition='top'
-                
-            >
-                <Text tabLabel='TOP'>My</Text>
-                <Text tabLabel='MOST RECENT'>favorite</Text>
-                <Text tabLabel='MY VIDEOS'>project</Text>
-                <Text tabLabel='COMMUNITY'>My</Text>
-            </ScrollableTabView>
+            <View>
+              <ScrollPlaceholder></ScrollPlaceholder>
+              <ScrollableTabView
+                  style={styles.ScrollableTabView}
+                  // initialPage={0}
+                  renderTabBar={() => <ScrollableTabBar />}
+                  tabBarPosition='top'
+                  
+              >
+                  <Text tabLabel='TOP'>My</Text>
+                  <Text tabLabel='MOST RECENT'>favorite</Text>
+                  <Text tabLabel='MY VIDEOS'>project</Text>
+                  <Text tabLabel='COMMUNITY'>My</Text>
+              </ScrollableTabView>
+            </View>
         );
     }
 }
 const styles = StyleSheet.create({
   ScrollableTabView: {
-    // position: "absolute",
-    // zIndex: 100,
+    position: "absolute",
+    zIndex: 100,
     backgroundColor: 'white',
-    height: 40
+    height: 50
   }
 })
+
+const ScrollPlaceholder = styled.View`
+  height: 50px;
+  position: relative;
+`
 
 
 export default ScrollableTab
