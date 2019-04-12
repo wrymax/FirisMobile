84f320daaba5af9aebbddb1aaaba3721752fee5c
Max Wang <wrymax@qq.com>
2019-04-12 13:43:01 -0400

    update style of my videos

--- app/components/MyVideoListItem.js
+++ app/components/MyVideoListItem.js
@@ -14,9 +14,11 @@ class MyVideoListItem extends React.Component {
     return (
       <TouchableOpacity onPress={this._onPress}>
         <ViewWrapper>
-          <Image source={{uri: video.item.image}} style={{width: '100%', height: 200}} />
+          <Image source={{uri: video.item.image}} style={{width: '40%', marginTop: 15, marginBottom: 15, marginLeft: 15}} />
           <MovieWrapper>
             <MovieName>{video.item.name}</MovieName>
+            <Timeago>{video.item.time}</Timeago>
+            <Status>{video.item.status}</Status>
           </MovieWrapper>
         </ViewWrapper>
       </TouchableOpacity>
@@ -26,17 +28,38 @@ class MyVideoListItem extends React.Component {
 
 const ViewWrapper = styled.View`
   backgroundColor: 'rgb(255,255,255)';
-  marginBottom: 20;
+  /* marginBottom: 20; */
+  borderBottomColor: #EEE;
+  borderBottomWidth: 1;
+  flex: 1;
+  flexDirection:row;
+  /* alignContent: center; */
+  /* justifyContent: center; */
+  height: 120;
+  overflow: hidden;
 `
 const MovieWrapper = styled.View`
   paddingLeft: 18;
   paddingTop: 20;
   paddingBottom: 20;
+  /* float: right; */
+  width: 50%;
 `
 const MovieName = styled.Text`
-  fontSize: 18;
-  
+  fontSize: 16;
   color: 'rgb(30,30,30)';
+`
+
+const Timeago = styled.Text`
+  fontSize: 12;
+  color: #777;
+  marginTop: 5;
+  marginBottom: 5;
+`
+
+const Status = styled.Text`
+  fontSize: 16;
+  color: #4366FF;
 `
 
 export default MyVideoListItem
