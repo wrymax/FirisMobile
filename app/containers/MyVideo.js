9a65de5dcdf7dd3e7c5f6a60ba1e88024f536cc5
shanwen <shawnwangchn@gmail.com>
2019-04-12 12:48:49 -0400

    init shanwen

--- app/containers/MyVideo.js
+++ app/containers/MyVideo.js
@@ -1,33 +1,75 @@
 import React, { Component } from "react"
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 import styled from 'styled-components'
 import BottomFloatButton from '../components/BottomFloatButton'
 import ScrollableTab from "../components/ScrollableTab";
 import { connect } from 'react-redux'
 import { createAction, NavigationActions } from '../utils'
+import fakeMovies from '../test_data/fakeMovies'
+import MovieListItem from '../components/MovieListItem'
 
-@connect(({ app }) => ({ ...app }))
+// @connect(({ app }) => ({ ...app }))
+@connect(({ app, Home }, router) => ({ app, Home, router }))
 class MyVideo extends Component {
+
     static navigationOptions = {
-        title: 'MyVideo',
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ focused, tintColor }) => (
+          <Image
+            style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
+            source={require('../images/house.png')}
+          />
+        ),
     }
 
-    pressNextButton = () => {
-        this.props.dispatch(NavigationActions.navigate({ routeName: 'Home' }))
+    // pressMovieItem = () => {
+    //     this.props.dispatch(NavigationActions.navigate({ routeName: 'Home' }))
+    // }
+
+    _pressMovieItem = (movie) => {
+        this.props.dispatch(NavigationActions.navigate({ routeName: 'MovieDetail', params: { movie: movie } }))
     }
+
+    renderMovieItem = (movie, id) => {
+        return (
+        <MovieListItem 
+            movie={movie}      
+            onPressItem={this._pressMovieItem}   
+        ></MovieListItem>
+        )
+    }
+    keyExtractor = item => item.name
 
     render() {
         return (
-            <View style={styles.button}>
+            // <View style={styles.button}>
+            //     <ScrollableTab initialPage={2}/>
+            //     <Text>My Video</Text>
+            //     <BottomFloatButton onPress={this.pressNextButton} />
+            // </View>
+            
+            <View>
                 <ScrollableTab initialPage={2}/>
-                <Text>My Video</Text>
-                <BottomFloatButton onPress={this.pressNextButton} />
+                <MovieList
+                    data={fakeMovies}
+                    renderItem={this.renderMovieItem}
+                    keyExtractor={this.keyExtractor}
+                >
+                </MovieList>
             </View>
+
         )
     }
 }
 
+const MovieList = styled.FlatList`
+`
+
 const styles = StyleSheet.create({
+    icon: {
+        width: 32,
+        height: 32,
+    },
     button: {
         flex: 1
     }
